e2a5d656dee9004ef22af5c1e27b5c83f6f088cc
MattiaDurante <119576786+MattiaDurante@users.noreply.github.com>
2023-03-11 12:19:05 +0100

    Add files via upload

--- src/esercitazione.java
+++ src/esercitazione.java
@@ -1,3 +1,4 @@
+package ciao;
 import java.lang.Math;
 import java.util.Scanner;
 
@@ -6,19 +7,25 @@ public class esercitazione
  static Scanner in = new Scanner(System.in);
 	public static void main(String[] args) 
 	{
+		primo_es();
+		secondo_es();
+
+	}
+	static void primo_es()
+	{
 		int n;
 		while (true)
 		{
 		System.out.println("di quanti numeri vuoi creare l'array? numero obbligatoriamente pari");
-		n = in.nextInt();
-		if (n % 2 == 0) /*controllo se Ë pari*/
+		n = Integer.parseInt(in.nextLine());
+		if (n % 2 == 0) /*controllo se √® pari*/
 			break;
 		else
 		System.out.println("numero OBBLIGATORIAMENTE pari :)");/*obbligatoriamente*/
 		}
 		double[] array = new double[n];
 		riempi_array_double(array);
-	double[] array2 = new double[n/2];/*creo un array con la met‡ dei numeri*/
+	double[] array2 = new double[n/2];/*creo un array con la met√† dei numeri*/
 	togli_decimali(array);
 	riempi_secondo(array, array2);
 	System.out.println("i numeri nel primo array sono:");
@@ -32,18 +39,19 @@ public class esercitazione
 	{
 		for(int i = 0; i < a.length; i++)/* incremento la i per poter scorrere l'array*/
 		{
-			a[i] = (Math.random() * 11); /* prendo in ingresso numeri casuali da 0 a 11(poca probabilit‡ che esca)*/
-		if (a[i] == 11) /*tolgo la probablit‡ che esca 11*/
+			a[i] = (Math.random() * 11); /* prendo in ingresso numeri casuali da 0 a 11(poca probabilit√† che esca)*/
+		if (a[i] == 11) /*tolgo la probablit√† che esca 11*/
 			i--;
 		}
 return a;	
 	}
 	static double[] riempi_secondo(double[]a, double[] b)
 	{
-		for (int i = 0; i < b.length; i++)
+		int c = 0;
+		for (int i = 0; i < a.length; i+=2)
 		{
-			b[i] = a[i] + a[i+1];
-		i++;
+			b[c] = a[i] * a[i+1];
+		c++;
 		}
 		
 		return b;
@@ -63,4 +71,37 @@ return a;
 		}
 		return a;
 		}
+	static void secondo_es()
+	{
+		System.out.println("quante stringhe vuoi scrivere?");
+		int n = Integer.parseInt(in.nextLine());
+		String[] a = new String[n]; 
+		String[] b = new String[n]; 
+		riempi_array(a);
+		riempi_array(b);
+		uguali(a,b);
+	}
+	static String[] riempi_array(String[] a)
+	{
+		for (int i = 0; i < a.length; i++)
+		{
+			System.out.println("scrivi la " + (i+1) + "¬∞ stringa");
+		a[i] = in.nextLine();	
+		}
+		return a;
+	}
+	static void uguali (String[]a, String[] b)
+	{
+	for(int i = 0; i < a.length; i++)
+		for(int j = 0; j < a.length; j++)
+			if (a[i].equals(b[j]))
+			{
+				System.out.println("OK");
+			System.exit(0);	
+			}
+	System.out.println("NO");
+	}
+	
 }
+
+
